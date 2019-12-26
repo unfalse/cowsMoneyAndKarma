f93fd0c46806c0aa90a57ba164fb4d3555d5bedc
Nopefish <fadeout01@gmail.com>
2019-12-26 12:22:49 +0500

    removed comments from utils

--- utils.js
+++ utils.js
@@ -7,29 +7,23 @@ Utils = {
       a_KEY: 65
     },
   
-    // event.type должен быть keypress
     getChar: function(event) {
       if (event.which == null) {  // IE
         if (event.keyCode < 32)
-          return null; // спец. символ
+          return null;
         return String.fromCharCode(event.keyCode) 
       }
   
-      if (event.which!=0 && event.charCode!=0) { // все кроме IE
-        if (event.which < 32) return null; // спец. символ
-          return String.fromCharCode(event.which); // остальные
+      if (event.which!=0 && event.charCode!=0) {
+        if (event.which < 32) return null;
+          return String.fromCharCode(event.which);
       }
   
-      return null; // спец. символ
+      return null;
     },
     
-    // использование Math.round() даст неравномерное распределение!
     getRandomInt: function(min, max)
     {
       return Math.floor(Math.random() * (max - min + 1)) + min;
-    },
-    
-    text: function(str){
-      console.log(str);
     }
   }
